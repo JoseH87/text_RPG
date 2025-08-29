3433099135d96277adbc1c1fbb52c662fef9ee69
josh <blindogs87@yahoo.es>
2025-08-29 14:42:15 +0200

    Character class initiate

--- text_rpg/src/main/java/com/example/Character.java
+++ text_rpg/src/main/java/com/example/Character.java
@@ -4,12 +4,17 @@ public abstract  class Character {
 
     //Atributtes
     private String name;
-
-
+    private int health;
+    private String sex;
 
     //Methods
-    
-    
-    
+    public abstract void talk();
+
+    public abstract void walk();
+
+    public abstract void sleep();
+
+    public abstract void eat();
+
 }
 
